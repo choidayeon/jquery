8bdbb84952b8ad3aca8dd6d8cfd1cb98af16c105
choidayeon <41932112+choidayeon@users.noreply.github.com>
2018-10-30 12:02:27 +0900

    plus

--- js/main.js
+++ js/main.js
@@ -28,14 +28,14 @@ $(function(){
 		$(".banner").css({left:current*width*-1});
 	});
 
-	$("#next_btn").click(function(){
+	$(".next").click(function(){
 		if(current<=count-1){
 			current++;
 			if(current==count){current=0};
 		}
 		listMove();
 	});
-	$("#prev_btn").click(function(){
+	$(".prev").click(function(){
 		if(current>-1){
 			current--;
 			if(current==-1){current=count-1};
@@ -46,4 +46,15 @@ $(function(){
 	function listMove(){
 		$(".banner").stop().animate({left:current*width*-1});
 	};
+
+	var card = $(".container #section");
+	var cardPos = card.offset().top;
+
+	$(window).scroll(function(){
+		var scrollY = window.pageYOffset;
+
+		if(scrollY>cardPos-1000){
+			card.stop().slideDown();
+		}
+	});
 });
